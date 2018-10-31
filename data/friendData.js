3b2b37950154d9337b72f2e760e147dff30f1a96
Phoenix <aja.magdaleno@gmail.com>
2018-10-30 17:48:46 -0700

    w/ new api route

---
+++ data/friendData.js
@@ -0,0 +1,51 @@
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+
+var friendArray = [
+    {
+      name: "Kanye West",
+      photo: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/10/12/08/Kanye-West-White-House.jpg",
+      scores: [ '1', '5'
+      // , '3', '2', '4', '5', '1', '3', '2', '4'
+              ]
+    },
+
+    {
+      name: "Hope Gratrix",
+      photo: "https://i.pinimg.com/originals/53/a3/02/53a302022a32b9a88a280f6e9ac74065.png",
+      scores: [ '5', '2'
+      //, '1', '4', '3', '3', '2', '6', '4', '2'
+              ]
+    },
+
+    
+    {
+      name: "Aja Magdaleno",
+      photo: "https://pics.clipartpng.com/midle/Brown_Bear_PNG_Clipart-669.png",
+      scores: [ '2', '3'
+      // , '2', '5', '1', '1', '5', '4', '5', '5'
+              ]
+    },
+
+    {
+      name: "Armando Pensado",
+      photo: "https://png.pngtree.com/element_origin_min_pic/17/01/05/afca771880b488c509d061392493efc0.jpg",
+      scores: [ '3', '4'
+      // , '5', '4', '2', '2', '1', '4', '2', '1'
+              ]
+    },
+
+  ];
+
+  var userArray = [
+    
+  ]
+  
+  // Export the array to make it accessible to other files by using 'require'.
+  module.exports = friendArray;
+ 
+
+
+
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,8 @@
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on all users input.
-
-var userData = require("../data/userData");
-
+// var userData = require("../data/userData");
+var friendArray = require("../data/friendData");
+var userArray = require("../data/userData");
 // ===============================================================================
 // ROUTING
 
@@ -16,24 +16,46 @@ module.exports = function(app) {
     let survey = req.body
     // req.json();
     userArray.push(survey)
-    console.log(survey)
-})
+    var userScores = survey.scores;
+
+    // Compute best friend match
+		var matchName = '';
+		var matchImg = '';
+		var randDiff = 10000; // Make an initial value big for comparison
+
+		//To loop through friends in the friendArray
+		for (var i = 0; i < friendArray.length; i++) {
+      //console.log('friend = ' + JSON.stringify(friendArray[i]));
+    
+      // Compute differenes for each question
+			var diff = 0;
+			for (var j = 0; j < userScores.length; j++) {
+				diff += Math.abs(parseInt(friendArray[i].scores[j]) - parseInt(userScores[j]));
+			}
+      console.log(diff);
+      console.log(friendArray)
+      console.log(userArray)
+      //If lowest difference, record the friend match
+			// if (diff < randDiff) {
+			// 	console.log('Match found via score diff: ' + diff);
+			// 	console.log('Match name = ' + friendArray[i].name);
+			// 	console.log('Match image = ' + friendArray[i].photo);
+
+			// 	randDiff = diff;
+			// 	matchName = friendArray[i].name;
+			// 	matchImg = friendArray[i].photo;
+			// }
+
+}
+  })
 
 //   app.get("/survey", function(req, res) {
 //   res.json(userData);
 // });
 
   app.get("/userData", function(req, res) {
-    res.json(userData);
+    res.json(friendArray);
   });
 
-
-
-
-//the POST route allows user data to be put onto the server
-    // app.post('/data/userData' , function (res, req) {
-    //     res.json(userData);
-    // })
-    
 }
 
